431c4c1be7bcb1223ac82e6e0bb40fbecc625986
ganesh Marmat <ganeshmarmat@gmail.com>
2019-11-24 23:37:16 +0530

    first drop with UI and service Basic

---
+++ src/ContactApp/ContactApp/Controllers/ContactController.cs
@@ -0,0 +1,69 @@
+﻿using ContactApp.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ContactApp.Controllers
+{
+    public class ContactController : Controller
+    {
+        static List<ContactDetails> lstcd = new List<ContactDetails>();
+        static ContactController()
+        {
+            lstcd.Add(new ContactDetails { Id = "1", FirstName = "ganesh", LastName = "marmat", Email = "ganeshmarmat@gamil.com", PhoneNumber = "12341234", Status = Status.Active });
+            lstcd.Add(new ContactDetails { Id = "2", FirstName = "ganesh", LastName = "marmat", Email = "ganeshmarmat@gamil.com", PhoneNumber = "12341234", Status = Status.Active });
+            lstcd.Add(new ContactDetails { Id = "3", FirstName = "ganesh", LastName = "marmat", Email = "ganeshmarmat@gamil.com", PhoneNumber = "12341234", Status = Status.Active });
+            lstcd.Add(new ContactDetails { Id = "4", FirstName = "ganesh", LastName = "marmat", Email = "ganeshmarmat@gamil.com", PhoneNumber = "12341234", Status = Status.Active });
+            lstcd.Add(new ContactDetails { Id = "5", FirstName = "ganesh", LastName = "marmat", Email = "ganeshmarmat@gamil.com", PhoneNumber = "12341234", Status = Status.Active });
+            lstcd.Add(new ContactDetails { Id = "6", FirstName = "ganesh", LastName = "marmat", Email = "ganeshmarmat@gamil.com", PhoneNumber = "12341234", Status = Status.Active });
+            lstcd.Add(new ContactDetails { Id = "7", FirstName = "ganesh", LastName = "marmat", Email = "ganeshmarmat@gamil.com", PhoneNumber = "12341234", Status = Status.Active });
+        }
+
+        public ActionResult Edit(string id)
+        {
+            return View(lstcd.FirstOrDefault(x => x.Id == id));
+        }
+        [HttpPost]
+        public ActionResult Edit(ContactDetails cd)
+        {
+            if (ModelState.IsValid)
+            {
+                lstcd[lstcd.IndexOf(lstcd.FirstOrDefault(x => x.Id == cd.Id))] = cd;
+                return RedirectToAction("Index");
+            }
+            return View(cd);
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(ContactDetails contactDetails)
+        {
+            if (ModelState.IsValid)
+            {
+                lstcd.Add(contactDetails);
+                return RedirectToAction("index");
+            }
+            return View(contactDetails);
+        }
+        // GET: Contact
+        public ActionResult Index()
+        {
+            return View(lstcd);
+        }
+        public ActionResult Delete(string id)
+        {
+            return View(lstcd.FirstOrDefault(x => x.Id == id));
+        }
+        [HttpPost]
+        public ActionResult Delete(ContactDetails contactDetails)
+        {
+            lstcd.RemoveAll(x => x.Id == contactDetails.Id);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ src/ContactApp/ContactApp/Models/ContactDetails.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ContactApp.Models
+{
+    public class ContactDetails
+    {
+        public string Id { get; set; }
+        [Required]
+        [DisplayName("First Name")]
+        public string FirstName { get; set; }
+        [DisplayName("Last Name")]
+        [Required]
+        public string LastName { get; set; }
+        [EmailAddress]
+        [Required]
+        public string Email { get; set; }
+        [DisplayName("Phone Number")]
+        [Phone]
+        [Required]
+        public string PhoneNumber{ get; set; }
+        [Required]
+        public Status Status { get; set; }
+    }
+    public enum Status{
+        Active,
+        Inactive
+    }
+}
